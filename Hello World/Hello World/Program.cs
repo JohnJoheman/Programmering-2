4cb903640e4f2d3d8e89f3542cc4442b7293d6b9
JohnJoheman <john.joheman@elev.ga.ntig.se>
2020-08-27 12:09:43 +0200

    Uppgift.1
    Variabler och Datatyper i C#

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -6,7 +6,66 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas!");
+           
+
+            Console.WriteLine("Hello Stranger!");
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age;");
+            var age = Console.ReadLine();
+
+
+            Console.WriteLine("Are you dead or alive? enter 1 for yes and 2 for no");
+
+            bool deadOrNot = true;
+            var yeet = Console.ReadLine();
+
+
+            if (yeet == "1"){
+                deadOrNot = true;
+
+            }
+            else
+            {
+                deadOrNot = false;
+            }
+
+            if (deadOrNot == true) 
+            {
+                Console.WriteLine("You won't be for too long tho >:)");
+                
+            }
+            else
+            {
+                Console.WriteLine("Well that's just highly unfortunate :(");
+                
+            }
+
+            
+
+
+            Console.WriteLine("Your name is: " + name);
+            Console.WriteLine("Your age is: " + age);
+
+            if(deadOrNot == true)
+            {
+                Console.WriteLine("Life-Status: Alive");
+            }
+            else
+            {
+                Console.WriteLine("Life-Status: Dead");
+            }
+            
+           
+            
+
+            
+            
+
+
+
         }
+
     }
 }
---
+++ Uppgift.1/Uppgift.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string val;
+            Console.Write("Please enter your name:");
+            
+            val = Console.ReadLine();
+
+            Console.WriteLine(val);
+        }
+    }
+}
