e1f1ecc1228493e389eccdd737ae77aa3314cffe
JohnJoheman <john.joheman@elev.ga.ntig.se>
2020-09-03 11:03:27 +0200

    Labb Villkor och Loopar

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -12,39 +12,58 @@ namespace Hello_World
             Console.WriteLine("Please enter your name:");
             string name = Console.ReadLine();
 
+            bool inputForName = true;
+
+            while (inputForName)
+            {
+                if (name != "")
+                {
+                    inputForName = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter your name");
+                    name = Console.ReadLine();
+                }
+            }
+
             Console.WriteLine("Please enter your age;");
             var age = Console.ReadLine();
 
-
-            Console.WriteLine("Are you dead or alive? enter 1 for yes and 2 for no");
+            
 
             bool deadOrNot = true;
-            var yeet = Console.ReadLine();
+            Console.WriteLine("Are you dead or alive? enter 1 for yes and 2 for no");
+
+            
+
+            var yeet = Convert.ToInt32(Console.ReadLine());
 
 
-            if (yeet == "1"){
-                deadOrNot = true;
+            switch (yeet)
 
-            }
-            else
             {
-                deadOrNot = false;
+                case 1:
+                    deadOrNot = true;
+                    break;
+
+                case 2:
+                    deadOrNot = false;
+                    break;
             }
 
             if (deadOrNot == true) 
             {
                 Console.WriteLine("You won't be for too long tho >:)");
-                
             }
             else
             {
-                Console.WriteLine("Well that's just highly unfortunate :(");
-                
+                Console.WriteLine("Well that's just highly unfortunate :("); 
             }
 
-            
 
 
+            Console.WriteLine();
             Console.WriteLine("Your name is: " + name);
             Console.WriteLine("Your age is: " + age);
 
@@ -56,9 +75,21 @@ namespace Hello_World
             {
                 Console.WriteLine("Life-Status: Dead");
             }
-            
-           
-            
+
+            Console.WriteLine();
+            Console.WriteLine("Type in 5 words");
+
+            var words = new String[5];
+            for (int i = 0; i < 5; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Your 5 words: ");
+            foreach (string word in words)
+
+   
+            Console.WriteLine(word);
 
             
             
