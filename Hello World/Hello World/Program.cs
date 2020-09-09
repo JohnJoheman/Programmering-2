cdb406b44362e5897de80f1b62d59410f581f2c8
JohnJoheman <john.joheman@elev.ga.ntig.se>
2020-09-09 21:32:15 +0200

    Arrays och listor
    Matar in 5 namn och skriver sedan ut dem vanligt och baklänges

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -6,93 +6,29 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-           
-
-            Console.WriteLine("Hello Stranger!");
-            Console.WriteLine("Please enter your name:");
-            string name = Console.ReadLine();
-
-            bool inputForName = true;
-
-            while (inputForName)
-            {
-                if (name != "")
-                {
-                    inputForName = false;
-                }
-                else
-                {
-                    Console.WriteLine("Please enter your name");
-                    name = Console.ReadLine();
-                }
-            }
-
-            Console.WriteLine("Please enter your age;");
-            var age = Console.ReadLine();
-
-            
-
-            bool deadOrNot = true;
-            Console.WriteLine("Are you dead or alive? enter 1 for yes and 2 for no");
-
-            
-
-            var yeet = Convert.ToInt32(Console.ReadLine());
-
-
-            switch (yeet)
-
-            {
-                case 1:
-                    deadOrNot = true;
-                    break;
-
-                case 2:
-                    deadOrNot = false;
-                    break;
-            }
-
-            if (deadOrNot == true) 
-            {
-                Console.WriteLine("You won't be for too long tho >:)");
-            }
-            else
-            {
-                Console.WriteLine("Well that's just highly unfortunate :("); 
-            }
-
-
-
-            Console.WriteLine();
-            Console.WriteLine("Your name is: " + name);
-            Console.WriteLine("Your age is: " + age);
-
-            if(deadOrNot == true)
-            {
-                Console.WriteLine("Life-Status: Alive");
-            }
-            else
-            {
-                Console.WriteLine("Life-Status: Dead");
-            }
-
-            Console.WriteLine();
+          
             Console.WriteLine("Type in 5 words");
 
             var words = new String[5];
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < words.Length; i++)
             {
                 words[i] = Console.ReadLine();
             }
+
             Console.WriteLine();
             Console.WriteLine("Your 5 words: ");
             foreach (string word in words)
-
-   
             Console.WriteLine(word);
 
+            Console.WriteLine();
+            Console.WriteLine("Here are your words but printed backwards: ");
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+            }
             
             
+
 
 
 
---
+++ Test/Test/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Test
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello Stranger!");
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            bool inputForName = true;
+
+            while (inputForName)
+            {
+                if (name != "")
+                {
+                    inputForName = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter your name");
+                    name = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("Please enter your age;");
+            var age = Console.ReadLine();
+
+
+
+            bool deadOrNot = true;
+            Console.WriteLine("Are you dead or alive? enter 1 for yes and 2 for no");
+
+
+
+            var yeet = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (yeet)
+
+            {
+                case 1:
+                    deadOrNot = true;
+                    break;
+
+                case 2:
+                    deadOrNot = false;
+                    break;
+            }
+
+            if (deadOrNot == true)
+            {
+                Console.WriteLine("You won't be for too long tho >:)");
+            }
+            else
+            {
+                Console.WriteLine("Well that's just highly unfortunate :(");
+            }
+
+
+
+            Console.WriteLine();
+            Console.WriteLine("Your name is: " + name);
+            Console.WriteLine("Your age is: " + age);
+
+            if (deadOrNot == true)
+            {
+                Console.WriteLine("Life-Status: Alive");
+            }
+            else
+            {
+                Console.WriteLine("Life-Status: Dead");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Type in 5 words");
+
+            var words = new String[5];
+            for (int i = 0; i < 5; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Your 5 words: ");
+            foreach (string word in words)
+
+
+                Console.WriteLine(word);
+
+
+
+
+
+
+        }
+
+    }
+}
