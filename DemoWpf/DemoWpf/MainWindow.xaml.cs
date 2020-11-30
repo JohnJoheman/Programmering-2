93327b0870dc58a2055b51c02151e87dba77e623
JohnJoheman <john.joheman@elev.ga.ntig.se>
2020-11-30 20:05:26 +0100

    Färdig miniräknare
    Jag glömde helt av att comitta tidigare 0_o

--- DemoWpf/DemoWpf/MainWindow.xaml.cs
+++ DemoWpf/DemoWpf/MainWindow.xaml.cs
@@ -24,7 +24,125 @@ namespace DemoWpf
         {
             InitializeComponent();
         }
+        
+        private void NumberButton_click(object sender, RoutedEventArgs e)
+        {
+
+            
+            int[] numberList = { };
+            if (e.Source is Button button)
+            {
+                switch (button.Content)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                    case "-":
+                    case "+":
+                    case "*":
+                    case "/":
+                    case "^":
+                    case ",":
+                    case "√":
+                        InOutField.Text += button.Content;
+                        break;
+
+                    case "=":
+                        if (InOutField.Text.Contains('-'))
+                        {
+                            InOutField.Text = calculateAlgorythm('-');
+                        }
+
+                        else if (InOutField.Text.Contains('+'))
+                        {
+                            InOutField.Text = calculateAlgorythm('+');
+                        }
+
+                        else if (InOutField.Text.Contains('*'))
+                        {
+                            InOutField.Text = calculateAlgorythm('*');
+                        }
+
+                        else if (InOutField.Text.Contains('/'))
+                        {
+                            InOutField.Text = calculateAlgorythm('/');
+                        }
+
+                        else if (InOutField.Text.Contains('^'))
+                        {
+                            InOutField.Text = calculateAlgorythm('^');
+                        }
+
+                        else if (InOutField.Text.Contains(','))
+                        {
+                            InOutField.Text = calculateAlgorythm(',');
+                        }
+
+                        else if (InOutField.Text.Contains('√'))
+                        {
+                            InOutField.Text = calculateAlgorythm('√');
+                        }
+                        break;
+
+                    case "Clear":
+                        InOutField.Text = "";
+                        break;
+
+                    default:
+                        break;
+    
+                }
+            }
+        }
+        private string calculateAlgorythm(char calculations)
+        {
+            var result = 0.0;
+            var numberHolder = InOutField.Text.Split('+', '-', '*', '/', '^', '√');
+            if (numberHolder[0] == "")
+            {
+                numberHolder[0] = "1";
+            }
+            
+            var firstNumber = Convert.ToDouble(numberHolder[0]);
+            var secondNumber = Convert.ToDouble(numberHolder[1]);
 
 
+            switch (calculations)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+
+                case '/':
+                    result = firstNumber / secondNumber;
+                    break;
+
+                case '^':
+                    result = Math.Pow(firstNumber, secondNumber);
+                    break;
+
+                case '√':
+                    result = Math.Sqrt(secondNumber);
+                    break;
+
+            }
+            var resultView = Convert.ToString(result);
+            return resultView;
+        }
     }
 }
