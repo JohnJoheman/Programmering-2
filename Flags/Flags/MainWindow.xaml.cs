e003b19886e757c11811b7cd066ddc50e3a7e80a
JohnJoheman <john.joheman@elev.ga.ntig.se>
2021-03-25 17:27:59 +0100

    Repetition WPF (flags)

---
+++ Flags/Flags/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flags
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Knapp_Click(object sender, RoutedEventArgs e)
+        {
+            if (KnappText.Text == "Skala")
+            {
+                KnappText.Text = "Banan";
+                Knapp.Content = "Skala";
+            }
+            else if(KnappText.Text == "Banan")
+            {
+                var newButton = new Button();
+                newButton.Content = "Ny Knapp";
+                newButton.Height = 30;
+                newButton.Width = 75;
+                MainGrid.Children.Add(newButton);
+            }
+            else
+            {
+                KnappText.Text = "Skala";
+            }
+            
+        }
+    }
+}
